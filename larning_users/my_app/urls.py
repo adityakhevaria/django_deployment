2d417e520bc7d651f5d3246d01db7e35e429af89
Aditya Khevaria <adityakhevaria@gmail.com>
2023-10-07 15:21:01 +0530

    first commit

---
+++ larning_users/my_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from my_app import views
+
+app_name="my_app"
+
+urlpatterns=[
+    path('register/',views.register,name='register'),
+    path("user_login/",views.user_login,name="user_login"),
+    
+]
